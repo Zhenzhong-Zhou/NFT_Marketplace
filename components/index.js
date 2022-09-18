cf9835fc9848801ce2ad5152f1e896514b5f310d
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-17 22:03:41 -0600

    Add CreateCard and best creators

--- components/index.js
+++ components/index.js
@@ -1,7 +1,7 @@
-// eslint-disable-next-line import/no-cycle
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Button from './Button';
 import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Navbar, Footer, Button, Banner };
+export { Navbar, Footer, Button, Banner, CreatorCard };
