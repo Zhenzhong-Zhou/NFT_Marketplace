db1e88068b6fc05959f205c9136f74a3312e63bd
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-18 21:14:30 -0600

    Add create token

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -49,5 +49,12 @@ contract NFTMarketplace is ERC721URIStorage.sol {
         return listingPrice;
     }
 
-    
+    function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        createMarketItem(newTokenId, price);
+        return newTokenId;
+    }
 }
