7c531874960bf8e8351435a13437fa279cfa8f15
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-18 22:37:30 -0600

    Deploy smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-require('@nomicfoundation/hardhat-toolbox');
+const fs = require('fs');
+
+require('@nomiclabs/hardhat-waffle');
+
+const privateKey = fs.readFileSync('.secret').toString().trim();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    networks: {
+        hardhat: {
+            chainId: 1337,
+        },
+    },
     solidity: '0.8.17',
 };
