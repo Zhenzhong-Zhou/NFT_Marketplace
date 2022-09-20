07193b8fffdfe5fde0178447dfaaa0267df6f28a
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-19 20:06:35 -0600

    Fix errors: React 18: Hydration failed because the initial UI does not match what was rendered on the server

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
@@ -5,19 +6,34 @@ import { NFTProvider } from '../context/NFTContext';
 import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => (
-    <NFTProvider>
-        <ThemeProvider attribute="class">
-            <div className="dark:bg-nft-dark bg-white min-h-screen">
-                <Navbar />
-                <div className="pt-65">
-                    <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+    const [showing, setShowing] = useState(false);
+
+    useEffect(() => {
+        setShowing(true);
+    }, []);
+
+    if (!showing) {
+        return null;
+    }
+
+    if (typeof window === 'undefined') {
+        return <></>;
+    }
+    return (
+        <NFTProvider>
+            <ThemeProvider attribute="class">
+                <div className="dark:bg-nft-dark bg-white min-h-screen">
+                    <Navbar />
+                    <div className="pt-65">
+                        <Component {...pageProps} />
+                    </div>
+                    <Footer />
+                    <Script src="https://kit.fontawesome.com/e3d41100ae.js" crossOrigin="anonymous" />
                 </div>
-                <Footer />
-                <Script src="https://kit.fontawesome.com/e3d41100ae.js" crossOrigin="anonymous" />
-            </div>
-        </ThemeProvider>
-    </NFTProvider>
-);
+            </ThemeProvider>
+        </NFTProvider>
+    );
+};
 
 export default MyApp;
