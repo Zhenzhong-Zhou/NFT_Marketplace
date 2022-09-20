82faaca4ff7129f8297547a42fbbfaed75c6adf9
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-19 23:02:52 -0600

    Add my NFTs page

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -6,7 +6,6 @@ import images from '../assets';
 const Loader = () => (
     <div className="flexCenter w-full my-4">
         <Image src={images.loader} alt="loader" width={100} objectFit="contain" />
-        {/* <div className={""} */}
     </div>
 );
 
