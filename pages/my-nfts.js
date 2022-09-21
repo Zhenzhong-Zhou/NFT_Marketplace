44f6bb8bfb434711390d22d4fd09e56b60336e1c
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-20 22:47:21 -0600

    Add search bar component

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -3,20 +3,39 @@ import Image from 'next/image';
 
 import { NFTContext } from '../context/NFTContext';
 import images from '../assets';
-import { Banner, Loader, NFTCard } from '../components';
+import { Banner, Loader, NFTCard, SearchBar } from '../components';
 import { shortenAddress } from '../utils/shortenAddress';
 
 const MyNFTs = () => {
     const { fetchMyNFTsOrListedNFTs, currentAccount } = useContext(NFTContext);
     const [nfts, setNfts] = useState([]);
+    const [nftsCopy, setNftsCopy] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [activeSelect, setActiveSelect] = useState('Recently Added');
 
     useEffect(() => {
         fetchMyNFTsOrListedNFTs().then((items) => {
             setNfts(items);
+            setNftsCopy(items);
             setIsLoading(false);
         });
     }, []);
+
+    useEffect(() => {
+        const sortedNfts = [...nfts];
+
+        switch (activeSelect) {
+        case 'Price (low to high)':
+            setNfts(sortedNfts.sort((a, b) => (a.price - b.price)));
+            break;
+        case 'Price (high to low)':
+            setNfts(sortedNfts.sort((a, b) => (b.price - a.price)));
+            break;
+        case 'Recently Added':
+            setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+            break;
+        }
+    }, [activeSelect]);
 
     if (isLoading) {
         return (
@@ -25,6 +44,22 @@ const MyNFTs = () => {
             </div>
         );
     }
+
+    const onHandleSearch = (value) => {
+        const filteredNFTs = nfts.filter(({ name }) => name.toLowerCase().includes(value.toLowerCase()));
+
+        if (filteredNFTs.length) {
+            setNfts(filteredNFTs);
+        } else {
+            setNfts(nftsCopy);
+        }
+    };
+
+    const onClearSearch = () => {
+        if (nfts.length && nftsCopy.length) {
+            setNfts(nftsCopy);
+        }
+    };
 
     return (
         <div className="w-full flex justify-start items-center flex-col min-h-screen">
@@ -41,7 +76,7 @@ const MyNFTs = () => {
                 </div>
             </div>
 
-            {!isLoading && !nfts.length ? (
+            {!isLoading && !nfts.length && !nftsCopy ? (
                 <div className="flexCenter sm:p-4 p-16">
                     <h1 className="font-poppins dark:text-white text-nft-black-1 font-extrabold text-3xl">
                         No NFTs Owned
@@ -50,7 +85,10 @@ const MyNFTs = () => {
             ) : (
                 <div className="sm:px-4 p-12 w-full minmd:w-4/5 flexCenter flex-col">
                     <div className="flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8">
-                        Search Bar
+                        <SearchBar
+                          activeSelect={activeSelect} setActiveSelect={setActiveSelect}
+                          handleSearch={onHandleSearch} clearSearch={onClearSearch}
+                        />
                     </div>
 
                     <div className="mt-3 w-full flex flex-wrap">
