fbf6ad17a1fe401be94db0b76898bb98025ec9e1
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-22 03:24:48 -0600

    Update wallet

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import market from './NFTMarketplace.json';
 
-export const MarketAddress = '0xC3a6f6FA0540df0d4074940400d473adE0EF301E';
+export const MarketAddress = '0x89dA358A2E45D892e2C13cb9a439e2f1067ED1ac';
 export const MarketAddressABI = market.abi;
