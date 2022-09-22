37e6d63a7f953d64739b5abf63357500e6e63e6d
Zhenzhong Zhou <bob.zhou@usask.ca>
2022-09-22 03:00:08 -0600

    Fix bugs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: 'zzz-nft-marketplace.infura-ipfs.io',
+		domains: ['zzz-nft-marketplace.infura-ipfs.io'],
 	},
 };
 
